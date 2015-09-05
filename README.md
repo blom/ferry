60c817987eef31a8ea924d4db0596fc5ff5cbacb
Ørjan Blom <blom@blom.tv>
2015-09-05 20:43:29 +0200

    Add the remaining resources

--- README.md
+++ README.md
@@ -5,8 +5,6 @@ ferry
 
 Clojure library for [DigitalOcean's API][digitalocean-api].
 
-Note that only droplet endpoints are supported currently.
-
 [travis]: https://travis-ci.org/blom/ferry
 [digitalocean-api]: https://developers.digitalocean.com/documentation/v2/
 
@@ -15,6 +13,25 @@ Examples
 
 ``` clojure
 (require '[ferry.client :as ferry])
+
+(ferry/account token)
+
+(ferry/actions token)
+(ferry/actions token {:per_page 1 :page 1})
+(ferry/action token action-id)
+
+(ferry/domains token)
+(ferry/domains token {:per_page 1 :page 1})
+(ferry/create-domain token {:name name :ip_address ip-address})
+(ferry/domain token domain-name)
+(ferry/delete-domain token domain-name)
+
+(ferry/records token domain-name)
+(ferry/records token domain-name {:per_page 1 :page 1})
+(ferry/create-record token domain-name {:type type :name name :data data})
+(ferry/record token domain-name record-id)
+(ferry/update-record token domain-name record-id {:data data})
+(ferry/delete-record token domain-name record-id)
 
 (ferry/create-droplet token {:name name
                              :image image
@@ -34,6 +51,28 @@ Examples
 (ferry/droplet-upgrades token)
 (ferry/droplet-action token droplet-id :disable_backups)
 (ferry/droplet-action token droplet-id :resize {:size size})
+
+(ferry/images token)
+(ferry/images token {:type "application"})
+(ferry/images token {:type "distribution"})
+(ferry/images token {:private true})
+(ferry/images token {:per_page 1 :page 1})
+(ferry/image token image-id-or-slug)
+(ferry/update-image token image-id {:name "foo"})
+(ferry/delete-image token image-id)
+(ferry/image-action token image-id action-id)
+(ferry/image-action token image-id :transfer {:region "ams2"})
+
+(ferry/ssh-keys token)
+(ferry/ssh-keys token {:per_page 1 :page 1})
+(ferry/create-ssh-key token {:name name :public_key public-key})
+(ferry/ssh-key token key-id-or-fingerprint)
+(ferry/update-ssh-key token key-id-or-fingerprint {:name "key name"})
+(ferry/delete-ssh-key token key-id-or-fingerprint)
+
+(ferry/regions token)
+
+(ferry/sizes token)
 ```
 
 All functions return a future and must be dereferenced to get the result:
--- src/ferry/client.clj
+++ src/ferry/client.clj
@@ -21,6 +21,60 @@
                                 "Content-Type" "application/json"}
                       :body (and params (json/write-str params))
                       :timeout 10000}))))
+(defn account
+  [token]
+  (request token :get "/account"))
+
+(defn actions
+  [token & [query-params]]
+  (request token :get "/actions" nil query-params))
+
+(defn action
+  [token action-id]
+  (request token :get (str "/actions/" action-id)))
+
+(defn domains
+  [token & [query-params]]
+  (request token :get "/domains" nil query-params))
+
+(defn create-domain
+  [token params]
+  (request token :post "/domains" params))
+
+(defn domain
+  [token domain-name]
+  (request token :get (str "/domains/" domain-name)))
+
+(defn delete-domain
+  [token domain-name]
+  (request token :delete (str "/domains/" domain-name)))
+
+(defn records
+  [token domain-name & [query-params]]
+  (request token
+           :get
+           (str "/domains/" domain-name "/records")
+           nil
+           query-params))
+
+(defn create-record
+  [token domain-name params]
+  (request token :post (str "/domains/" domain-name "/records") params))
+
+(defn record
+  [token domain-name record-id]
+  (request token :get (str "/domains/" domain-name "/records/" record-id)))
+
+(defn update-record
+  [token domain-name record-id params]
+  (request token
+           :put
+           (str "/domains/" domain-name "/records/" record-id)
+           params))
+
+(defn delete-record
+  [token domain-name record-id]
+  (request token :delete (str "/domains/" domain-name "/records/" record-id)))
 
 (defn create-droplet
   [token params]
@@ -74,3 +128,68 @@
            :post
            (str "/droplets/" droplet-id "/actions")
            (assoc params :type action)))
+
+(defn images
+  [token & [query-params]]
+  (request token :get "/images" nil query-params))
+
+(defn image
+  [token image-id-or-slug]
+  (request token :get (str "/images/" image-id-or-slug)))
+
+(defn image-actions
+  [token image-id]
+  (request token :get (str "/images/" image-id "/actions")))
+
+(defn update-image
+  [token image-id params]
+  (request token :put (str "/images/" image-id) params))
+
+(defn delete-image
+  [token image-id]
+  (request token :delete (str "/images/" image-id)))
+
+(defmulti image-action
+  (fn [& args]
+    (class (second (rest args)))))
+
+(defmethod image-action
+  java.lang.Long
+  [token image-id action-id]
+  (request token :get (str "/images/" image-id "/actions/" action-id)))
+
+(defmethod image-action
+  :default
+  [token image-id action & [params]]
+  (request token
+           :post
+           (str "/images/" image-id "/actions")
+           (assoc params :type action)))
+
+(defn ssh-keys
+  [token & [query-params]]
+  (request token :get "/account/keys" nil query-params))
+
+(defn create-ssh-key
+  [token params]
+  (request token :post "/account/keys" params))
+
+(defn ssh-key
+  [token key-id-or-fingerprint]
+  (request token :get (str "/account/keys/" key-id-or-fingerprint)))
+
+(defn update-ssh-key
+  [token key-id-or-fingerprint params]
+  (request token :put (str "/account/keys/" key-id-or-fingerprint) params))
+
+(defn delete-ssh-key
+  [token key-id-or-fingerprint]
+  (request token :delete (str "/account/keys/" key-id-or-fingerprint)))
+
+(defn regions
+  [token]
+  (request token :get "/regions"))
+
+(defn sizes
+  [token]
+  (request token :get "/sizes"))
